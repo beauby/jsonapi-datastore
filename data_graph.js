6a8569473b903b79185e814a0d45c2afc2f4c17c
Lucas Hosseini <lucas.hosseini@gmail.com>
2015-08-16 03:46:24 +0200

    Add base code.

---
+++ data_graph.js
@@ -0,0 +1,116 @@
+function DataGraphModel(type, id) {
+  this.id = id;
+  this._type = type;
+  this._attributes = [];
+  this._relationships = [];
+}
+
+DataGraphModel.prototype.serialize = function(opts) {
+  var self = this,
+      res = {},
+      key;
+
+  res.data = {
+    type: this._type,
+    id: this.id
+  };
+
+  if (this._attributes.length !== 0) res.data.attributes = [];
+  if (this._relationships.length !== 0) res.data.relationships = [];
+
+  this._attributes.forEach(function(key) {
+    res.data.attributes[key] = self[key];
+  });
+
+  this._relationships.forEach(function(key) {
+    if (self[key].constructor === Array) {
+      res.data.relationships[key] = {
+        data: self[key].map(function(rel) {
+          return { type: rel._type, id: rel.id };
+        })
+      };
+    } else {
+      res.data.relationships[key] = {
+        data: { type: self[key]._type, id: self[key].id }
+      };
+    }
+  });
+
+  return res;
+};
+
+function DataGraph() {
+  this.graph = {};
+}
+
+DataGraph.prototype.find = function(type, id) {
+  if (!this.graph[type] || !this.graph[type][id]) return null;
+  return this.graph[type][id];
+};
+
+DataGraph.prototype.initModel = function(type, id) {
+  this.graph[type] || (this.graph[type] = {});
+  this.graph[type][id] || (this.graph[type][id] = new DataGraphModel(type, id));
+
+  return this.graph[type][id];
+};
+
+DataGraph.prototype.syncRecord = function(rec) {
+  var self = this,
+      model = this.initModel(rec.type, rec.id),
+      key;
+
+  function findOrInit(resource) {
+    if (!self.find(resource.type, resource.id)) {
+      var placeHolderModel = self.initModel(resource.type, resource.id);
+      placeHolderModel._placeHolder = true;
+    }
+    return self.graph[resource.type][resource.id];
+  }
+
+  delete model._placeHolder;
+
+  for (key in rec.attributes) {
+    model._attributes.push(key);
+    model[key] = rec.attributes[key];
+  }
+
+  if (rec.relationships) {
+    for (key in rec.relationships) {
+      var rel = rec.relationships[key];
+      if (rel.data !== undefined) {
+        model._relationships.push(key);
+        if (rel.data === null) {
+          model[key] = null;
+        } else if (rel.data.constructor === Array) {
+          model[key] = rel.data.map(findOrInit);
+        } else {
+          model[key] = findOrInit(rel.data);
+        }
+      }
+      if (rel.links) {
+        console.log("Warning: Links not implemented yet.");
+      }
+    }
+  }
+
+  return model;
+};
+
+DataGraph.prototype.sync = function(data) {
+  var self = this;
+  function sync(data) {
+    if (!data) return null;
+    if (data.constructor === Array) {
+      return data.map(self.syncRecord.bind(self));
+    } else {
+      return self.syncRecord(data);
+    }
+  };
+  sync(data.included);
+  return sync(data.data);
+};
+
+DataGraph.prototype.reset = function() {
+  this.graph = {};
+};
